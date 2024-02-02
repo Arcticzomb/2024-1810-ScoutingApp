ff5d8965c408e53a7b0a34aec82b762ec439d9f3
Sebastian Smith <sss00122@ksad-fk43k41wfv.ks.k12itc.us>
2024-02-01 19:54:05 -0600

    start page design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,24 @@
 </svelte:head>
 
 <!-- TODO: Make page look nicer (please) -->
+<!-- I gotchu homie -->
+<div>
+    <img src="logo.png" alt="favicon.png" width="30%" height="30%" class="p-2 inline-block">
+    <span id="ampText" class="flex-col text-w scale-text font-semibold inline-block text-center px-3">
+        The Official 1810 & 9316<br>Scouting App</span>
+</div>
+
+
+
+<!-------------------->
 <div class="flex flex-col m-10 gap-5 items-center">
     <a href="/scouting" class="text-w rounded bg-active p-1 text-xl text-center" data-sveltekit-preload-data="off">Start Scouting</a>
     <a href="/data/ppg" class="text-w rounded bg-active p-1 text-xl">Data</a>
 </div>
+
+<style>
+    .scale-text {
+        font-size : 5vw;
+    }
+
+</style>
