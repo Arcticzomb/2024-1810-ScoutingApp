d7567d95ed641e59525c7c8e5d1d62f825c8605e
Sebastian Smith <sss00122@ksad-fk43k41wfv.ks.k12itc.us>
2024-02-17 13:10:31 -0600

    =Remove TBA_KEY stuff for vercel

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,6 @@ import {
     PUBLIC_SUPABASE_ANON_KEY,
     PUBLIC_FRC_USERNAME,
     PUBLIC_FRC_API_KEY,
-    PUBLIC_TBA_KEY
 } from "$env/static/public";
 import { ScoutingFetch } from "$lib/ScoutingFetch/ScoutingFetch";
 
@@ -18,7 +17,7 @@ export const handle: Handle = async ({ event, resolve }) => {
     });
 
     event.locals.scoutingFetch = new ScoutingFetch(2023, "mose", {
-        TBA: PUBLIC_TBA_KEY,
+        // TBA: PUBLIC_TBA_KEY,
         FIRSTEvents: {
             username: PUBLIC_FRC_USERNAME,
             key: PUBLIC_FRC_API_KEY
