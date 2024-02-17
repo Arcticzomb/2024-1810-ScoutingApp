c012dda55401db113d18c062370f8aba3b1e70a2
Sebastian Smith <sss00122@ksad-fk43k41wfv.ks.k12itc.us>
2024-02-17 13:19:06 -0600

    =added padding on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,9 @@
 </div>
 
 <!-------------------->
-<div class="flex flex-col m-10 gap-5 items-center h-full bg-primary rounded-lg p-3[">
-    <a href="/scouting" class="text-w rounded bg-active p-1 scale-text text-center" data-sveltekit-preload-data="off">Start Scouting</a>
-    <a href="/data/ppg" class="text-w rounded bg-active p-1 scale-text">Data</a>
+<div class="flex flex-col m-10 gap-5 items-center h-full bg-primary rounded-lg p-3">
+    <a href="/scouting" class="text-w rounded bg-active p-1 px-2 scale-text text-center" data-sveltekit-preload-data="off">Start Scouting</a>
+    <a href="/data/ppg" class="text-w rounded bg-active p-1 px-2 scale-text">Data</a>
 </div>
 
 <style>
