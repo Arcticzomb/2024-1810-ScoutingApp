b75afafcffe6c1ed05ddde2b808cd731fe313aa6
Bennett11123 <martiben000@usd232.or3>
2024-03-14 13:50:35 -0500

    Updated event code to heartland regional

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -16,7 +16,7 @@ export const handle: Handle = async ({ event, resolve }) => {
         event
     });
 
-    event.locals.scoutingFetch = new ScoutingFetch(2024, "FLWP", {
+    event.locals.scoutingFetch = new ScoutingFetch(2024, "KSLA", {
         // TBA: PUBLIC_TBA_KEY,`
         FIRSTEvents: {
             username: PUBLIC_FRC_USERNAME,
